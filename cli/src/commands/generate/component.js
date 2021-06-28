f4d38b29cab45ef7b0f332fa87ad56b3dac55a5c
Mir Carvalho <deppbrazil@gmail.com>
2021-06-28 20:41:53 -0300

    :rocket: Add commands in CLI

--- cli/src/commands/generate/component.js
+++ cli/src/commands/generate/component.js
@@ -1,21 +1,69 @@
 module.exports = {
-  name: 'generate',
-  alias: ['g'],
-  run: async toolbox => {
-    const {
-      parameters,
-      template: { generate },
-      print: { info }
-    } = toolbox
+  name: 'component',
+    description: 'Create new component',
+    run: async (toolbox) => {
+      const {
+        parameters,
+        template,
+        strings,
+        filesystem,
+        system,
+        print: { success, error },
+      } = toolbox;
 
-    const name = parameters.first
+      const project = filesystem.path(__dirname, '..', '..', '..', '..');
 
-    await generate({
-      template: 'model.js.ejs',
-      target: `models/${name}-model.js`,
-      props: { name }
-    })
+      const name = strings.pascalCase(parameters.first);
 
-    info(`Generated file at models/${name}-model.js`)
-  }
+      if (!name) {
+        error('🏴‍☠️ Hey marujo! você precisa dar um nome ao componente 🏴‍☠️');
+        return;
+      }
+
+      await template.generate({
+        template: 'component/package.json.ejs',
+        target: `${project}/packages/${name}/package.json`,
+        props: { name: strings.kebabCase(name) },
+      });
+
+      await template.generate({
+        template: 'component/babel.config.js.ejs',
+        target: `${project}/packages/${name}/babel.config.js`,
+      });
+
+      await template.generate({
+        template: 'component/jest.config.js.ejs',
+        target: `${project}/packages/${name}/jest.config.js`,
+      });
+
+      await template.generate({
+        template: 'component/lib/index.js.ejs',
+        target: `${project}/packages/${name}/lib/index.js`,
+        props: { name },
+      });
+
+      await template.generate({
+        template: 'component/lib/styles.js.ejs',
+        target: `${project}/packages/${name}/lib/styles.js`,
+        props: { name },
+      });
+
+      await template.generate({
+        template: 'component/docs/index.js.ejs',
+        target: `${project}/packages/${name}/docs/${name}.mdx`,
+        props: { name, route: strings.kebabCase(name) },
+      });
+
+      await template.generate({
+        template: 'component/__tests__/index.js.ejs',
+        target: `${project}/packages/${name}/__tests__/index.test.js`,
+        props: { name },
+      });
+
+      await system.run(`yarn prettier --write ${project}/packages/${name}`);
+
+      await system.run(`git checkout -b ${name}/0.1.0 master`);
+
+      success(`⚓ Componente ${name} criado! ⚓`);
+    },
 }
